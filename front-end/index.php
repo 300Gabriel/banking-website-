820278d8b75ce136877a3916f2204e8c86803691
GABRIEL <ogbagairukagabriel@gmail.com>
2026-06-20 13:38:40 +0100

    good thing to do so far

--- front-end/index.php
+++ front-end/index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="google-site-verification" content="36CNc69-uKVnpeIYfRdDXYbScmuQ1UjqfFU4k2MZZl4" />
   <title>TrustBank</title>
   <link rel="stylesheet" href="../css/components.css">
   <link rel="stylesheet" href="../css/landing.css">
